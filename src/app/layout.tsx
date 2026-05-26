93de5f44cd89e0c0d972968cf8f8090ab41b2080
Jenork <mrjenork@gmail.com>
2026-05-26 10:03:51 +0300

    talent verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="base:app_id" content="69e39c8642302cc6a1381d44" />
+
+        <meta
+          name="talentapp:project_verification"
+          content="27ed491a86187c7439278732681f825a0161531ba2b8caff4a39f2ac753be666353687f8df47f38e2b3bb4970b4d5d3be790e4d8eeb74058afbf71548c754623"
+        />
       </head>
+
       <body className="min-h-screen">
         <Providers>{children}</Providers>
       </body>
