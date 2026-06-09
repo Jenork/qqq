e6f1014de03b264242bc6caa63ad407cfb3b60fe
Jenork <mrjenork@gmail.com>
2026-06-09 15:04:29 +0300

    Use live season 2 arena background

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -7,7 +7,7 @@ export class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('arena-background', '/backgrounds/season2-station-v2.png')
+    this.load.image('arena-background', '/backgrounds/season2-station-live.png')
     preloadSpriteManifest(this.load)
   }
 
